c00071624f333baac6a71fa3ec4703f5cca68a20
kgw100 <kgw100@yonsei.ac.kr>
2022-07-06 03:43:35 +0900

    [V8] for문 버전으로~

--- star1_print.py
+++ star1_print.py
@@ -1,6 +1,11 @@
 # star1_print.py
+N = 6
+for level in range(0,N):
+    print(f" "*level+"*"*(N-level))
+
+# 참고용 amend 적용~
 print("******")
-print("*****")
+print(" *****")
 print(" ****")
 print("  ***")
 print("   **")
